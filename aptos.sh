760bd30379327bf5a5005c035f406082089d9d0e
007FLY <36874994+dislikediss@users.noreply.github.com>
2022-04-01 16:45:31 +0800

    Update aptos.sh

--- aptos.sh
+++ aptos.sh
@@ -1,94 +1,57 @@
-#!/usr/bin/env bash
-echo "首次使用aptos脚本，进行初始化……"
-sudo apt-get update
-sudo apt-get install -y jq
-sudo apt-get install -y lrzsz
-sudo apt-get install -y screen
-sudo apt-get install -y net-tools
-ulimit -n 65535
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt install curl -y < "/dev/null"
+fi
+curl -s https://api.nodes.guru/logo.sh | bash && sleep 2
+sudo apt update && sudo apt install git -y
+cd $HOME
+rm -rf aptos-core
+sudo mkdir -p /opt/aptos/data .aptos/config .aptos/key
+mv ~/aptos-node /usr/local/bin
+mv ~/aptos-operational-tool /usr/local/bin
+/usr/local/bin/aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file ~/.aptos/key/private-key.txt
+/usr/local/bin/aptos-operational-tool extract-peer-from-file --encoding hex --key-file ~/.aptos/key/private-key.txt --output-file ~/.aptos/config/peer-info.yaml &>/dev/null
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/config/src/config/test_data/public_full_node.yaml 
+cp ~/public_full_node.yaml ~/.aptos/config
+wget -O /opt/aptos/data/genesis.blob https://devnet.aptoslabs.com/genesis.blob
+wget -q -O ~/.aptos/waypoint.txt https://devnet.aptoslabs.com/waypoint.txt
+#wget -q -O /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+#wget -O seeds.yaml https://raw.githubusercontent.com/Pa1amar/aptos/main/seeds.yaml
+WAYPOINT=$(cat ~/.aptos/waypoint.txt)
+PRIVKEY=$(cat ~/.aptos/key/private-key.txt)
+PEER=$(sed -n 2p ~/.aptos/config/peer-info.yaml | sed 's/.$//')
+sed -i.bak -e "s/0:01234567890ABCDEFFEDCA098765421001234567890ABCDEFFEDCA0987654210/$WAYPOINT/" $HOME/.aptos/config/public_full_node.yaml
+sed -i "s/genesis_file_location: .*/genesis_file_location: \"\/opt\/aptos\/data\/genesis.blob\"/" $HOME/.aptos/config/public_full_node.yaml
+sleep 2 
+#/usr/local/bin/yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' $HOME/.aptos/config/public_full_node.yaml seeds.yaml
+sed -i "s/127.0.0.1/0.0.0.0/" $HOME/.aptos/config/public_full_node.yaml
+sed -i '/network_id: "public"$/a\
+      identity:\
+        type: "from_config"\
+        key: "'$PRIVKEY'"\
+        peer_id: "'$PEER'"' $HOME/.aptos/config/public_full_node.yaml
 
-echo "安装 Docker"
-# 安装 Docker
-wget -O get-docker.sh https://get.docker.com 
-sudo sh get-docker.sh
-rm -f get-docker.sh
+echo "[Unit]
+Description=Aptos
+After=network.target
 
-# 安装 docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/aptos-node -f $HOME/.aptos/config/public_full_node.yaml
+Restart=on-failure
+LimitNOFILE=65535
 
-# 安装 docker-compose
-
-echo "下载 Aptos 节点运行所需文件"
-mkdir -p ~/aptos-node && cd ~/aptos-node
-wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/docker-compose.yaml
-wget https://devnet.aptoslabs.com/genesis.blob
-wget https://devnet.aptoslabs.com/waypoint.txt
-
-echo "创建静态身份"
-docker run --rm aptoslab/tools:devnet sh -c "aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file /root/private-key.txt && aptos-operational-tool extract-peer-from-file --encoding hex --key-file /root/private-key.txt --output-file /root/peer-info.yaml && cat /root/private-key.txt && cat /root/peer-info.yaml" > key.txt
-sed -i '1,14d' key.txt
-sed -i '3,4d' key.txt
-sed -i '4d' key.txt
-sed -i 's/-//g' key.txt
-sed -i 's/ //g' key.txt
-sed -i 's/://g' key.txt
-
-#修改public_fulll_node.yaml内容
-cat key.txt | head -n 1 > privateKey.txt
-privateKey=$(cat key.txt | head -n 1)
-peerID=$(cat key.txt | tail -n +2 | head -n 1)
-cat>public_full_node.yaml<<EOF
-base:
-    # This is the location Aptos will store its database. It is backed by a dedicated docker volume
-    # for persistence.
-    data_dir: "/opt/aptos/data"
-    role: "full_node"
-    waypoint:
-        # This is a checkpoint into the blockchain for added security.
-        from_file: "/opt/aptos/etc/waypoint.txt"
-
-execution:
-    # Path to a genesis transaction. Note, this must be paired with a waypoint. If you update your
-    # waypoint without a corresponding genesis, the file location should be an empty path.
-    genesis_file_location: "/opt/aptos/etc/genesis.blob"
-
-full_node_networks:
-    - network_id: "public"
-      discovery_method: "onchain"
-      identity:
-        type: "from_config"
-        key: "${privateKey}"
-        peer_id: "${peerID}"
-      # The network must have a listen address to specify protocols. This runs it locally to
-      # prevent remote, incoming connections.
-      listen_address: "/ip4/127.0.0.1/tcp/6180"
-      # Define the upstream peers to connect to
-      seeds:
-        {}
-
-api:
-    # This specifies your REST API endpoint. Intentionally on public so that Docker can export it.
-    enabled: true
-    address: 0.0.0.0:8080
-EOF
-
-echo "添加原始节点"
-sudo wget -O /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64
-sudo chmod +x /usr/local/bin/yq
-wget https://raw.githubusercontent.com/dislikediss/Aptos_Full_node/main/seeds.yaml
-yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' ~/aptos-node/public_full_node.yaml seeds.yaml
-rm -rf seeds.yaml
-echo "显示public_full_node.yaml文本结果"
-cat public_full_node.yaml
-
-echo "开始运行"
-docker-compose up -d
-echo "读取私钥"
-cat key.txt | head -n 1
-echo "读取PeerID"
-cat key.txt | tail -n +2 | head -n 1
-sleep 20s
-echo "检查同步状态"
-curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type
-
+[Install]
+WantedBy=multi-user.target" > $HOME/aptosd.service
+mv $HOME/aptosd.service /etc/systemd/system/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable aptosd
+sudo systemctl restart aptosd
